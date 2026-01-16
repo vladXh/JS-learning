ab6555e136a685d0c8fdc47eacd2d1cace7d6247
Vlad <breahna.vlad@gmail.com>
2026-01-16 12:46:14 +0200

    Added js, made toggle button and made progress on calculator, close to finishing the input so it shows the input correctly

---
+++ script.js
@@ -0,0 +1,105 @@
+
+//////////////////////////////
+// Project 1: Toggle button //
+//////////////////////////////
+
+let toggled = false;
+document.getElementById("TestButton").addEventListener("click", function() {
+    const circleDiv = document.getElementById("TestCircleDiv");
+    
+    if (!toggled) {
+        circleDiv.style.backgroundColor = 'orangered';
+        toggled = true;
+    } else if (toggled) {
+        circleDiv.style.backgroundColor = 'rgb(' + 31 + ',' + 31 + ',' + 31 + ')';
+        toggled = false;
+    }
+
+});
+
+
+
+///////////////////////////
+// Project 2: Calculator //
+///////////////////////////
+
+// screen oveflow auto scroll 
+const screen = document.getElementById("CalculatorScreen");
+function updatescreen(value){
+    screen.scrollLeft = screen.scrollWidth;
+}
+
+const buttonContainer = document.getElementById("CalculatorButtons");
+let operators = [];
+let numbers = [];
+let hasDecimal = 0;
+
+buttonContainer.addEventListener("click", (e) => {
+    const btn = e.target.closest("button");
+    const screen = document.getElementById("CalculatorScreen");
+    const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    
+    if (!btn || !buttonContainer.contains(btn)) return;
+
+    if (btn.dataset.value !== undefined) {
+        console.log(btn.dataset.value);
+
+        // operators
+        if (btn.dataset.value === "+" || btn.dataset.value === "-" || btn.dataset.value === "*" || btn.dataset.value === "/") {
+            console.log("Operator button clicked");
+            hasDecimal = 0;
+
+            var lsatChar = screen.value.slice(-1);
+            if (lsatChar === "+" || lsatChar === "-" || lsatChar === "*" || lsatChar === "/") {
+                screen.value = screen.value.slice(0, -1);
+            }
+        } else if (btn.dataset.value === ".") {
+            hasDecimal++;
+            console.log("Decimal TRUE");
+            if (screen.value.slice(-1) === ".") {
+                screen.value = screen.value.slice(0, -1);
+            }
+        }
+
+        // numbers
+        else {
+            console.log("Number button clicked");
+        }
+
+        if (screen.value === "0" && btn.dataset.value !== ".") {
+            screen.value = "";
+        }
+        screen.value += btn.dataset.value;
+        updatescreen(btn.dataset.value);
+
+        if (btn.dataset.value === "%" || btn.dataset.value == "." && hasDecimal > 1) {
+            console.log(hasDecimal);
+            screen.value = screen.value.slice(0, -1);
+            hasDecimal--;
+        } else if (screen.value === "+" || screen.value === "*" || screen.value === "/") {
+            screen.value = "0";
+        }
+    } 
+    
+    // actions
+    else if (btn.dataset.action !== undefined) {
+        console.log(btn.dataset.action);
+        
+        if (btn.dataset.action === "clear") {
+            screen.value = "0";
+            hasDecimal = false;
+        } else if (btn.dataset.action === "delete") {
+            if (screen.value.slice(-1) === ".") {
+                hasDecimal = false;
+            }
+            screen.value = screen.value.slice(0, -1);
+
+        } else if (btn.dataset.action === "calculate") {
+            console.log("Calculating result...");
+            
+            hasDecimal = false;
+            operators = [];
+            numbers = [];
+        }
+    }
+});
